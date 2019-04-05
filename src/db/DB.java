67acded6e8bdd7b7f6ba6407edec782bd2683184
Gustavo Pavan <gustavopavang@gmail.com>
2019-04-04 23:30:11 -0300

    Reusing instantiation

--- src/db/DB.java
+++ src/db/DB.java
@@ -3,7 +3,6 @@ package db;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
